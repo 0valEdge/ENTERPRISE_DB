5f03ae9ee8d1590150272d9b164989f4738e3b07
kranthi.gogikar@ovaledge.com <kranthi.gogikar@ovaledge.com>
2026-04-28 07:22:41 +0000

    commit

---
+++ logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-04-28T07:07:47.970694716+00:00
+-- finished_at: 2026-04-28T07:07:48.043372427+00:00
+-- elapsed: 72ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c4018b-0619-9b2e-0000-c47560793eca
+-- desc: not provided
+select 1 as id;
